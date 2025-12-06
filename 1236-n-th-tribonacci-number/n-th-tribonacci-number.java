9c4ba233ec7627cce7c99a4a1d7d8a396683fca5
Lucky Baliyan <baliyanlucky85@gmail.com>
2025-12-06 13:31:44 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (48.77%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.java
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.java
@@ -3,7 +3,9 @@ class Solution {
         //return (int) tribonacciRec((long) n);
 
         long [] dp = new long [n+1];
-        return (int) tribonacciMemo(n,dp);
+        //return (int) tribonacciMemo(n,dp);
+
+        return (int) tribonacciTab(n,dp);
     }
 
    /* By Plain Recursion
@@ -15,7 +17,7 @@ class Solution {
     }
     */
 
-    public static long tribonacciMemo(int n,long [] dp){
+    /*public static long tribonacciMemo(int n,long [] dp){
         if(n<=0)return 0;
         if(n<=2)return 1;
 
@@ -26,6 +28,21 @@ class Solution {
 
         dp[n]  = tribonacciMemo(n-1,dp)+tribonacciMemo(n-2,dp)+
         tribonacciMemo(n-3,dp);
+        return dp[n];
+    }
+    */
+
+    public long tribonacciTab(int n,long [] dp){
+        if(n <= 0)return 0;
+        if(n<=2)return 1;
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 1;
+
+        for(int i = 3;i<=n;i++){
+            dp[i] = dp[i-1]+dp[i-2]+dp[i-3];
+        }
+
         return dp[n];
     }
 }
