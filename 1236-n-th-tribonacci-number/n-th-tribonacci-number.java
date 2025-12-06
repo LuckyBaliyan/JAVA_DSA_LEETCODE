1a596529bfad9aa22099ef638febe607abdb0eec
Lucky Baliyan <baliyanlucky85@gmail.com>
2025-12-06 13:23:31 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (48.77%) - LeetSync

---
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int tribonacci(int n) {
+        //return (int) tribonacciRec((long) n);
+
+        long [] dp = new long [n+1];
+        return (int) tribonacciMemo(n,dp);
+    }
+
+   /* By Plain Recursion
+    public static long tribonacciRec(long n){
+        if(n <= 0)return 0;
+        if(n == 1 || n == 2)return 1;
+
+        return tribonacciRec(n-1)+tribonacciRec(n-2)+tribonacciRec(n-3);
+    }
+    */
+
+    public static long tribonacciMemo(int n,long [] dp){
+        if(n<=0)return 0;
+        if(n<=2)return 1;
+
+        //check if we have the solution for the called subtree then return it 
+        // int O(1)
+
+        if(dp[n] != 0)return dp[n];
+
+        dp[n]  = tribonacciMemo(n-1,dp)+tribonacciMemo(n-2,dp)+
+        tribonacciMemo(n-3,dp);
+        return dp[n];
+    }
+}
