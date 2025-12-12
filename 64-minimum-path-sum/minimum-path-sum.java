cad2e98acfa7f47a08073da152cabb5d3dfc20e4
Lucky Baliyan <baliyanlucky85@gmail.com>
2025-12-12 18:02:10 +0530

    Time: 4 ms (62.13%) | Memory: 50.2 MB (43.40%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -6,9 +6,11 @@ class Solution {
         int [][] dp = new int [r][c];
 
         for(int [] arr:dp)Arrays.fill(arr,-1);
-        return solve(grid,r-1,c-1,dp);
+        return solve(grid,r,c,dp);
     }
 
+    // Memoization Approach --->
+    /*
     public static int solve(int [][] grid,int r,int c,int [][] dp){
         if(r < 0 || c < 0)return Integer.MAX_VALUE;
         if(r == 0 && c == 0)return grid[0][0];
@@ -19,5 +21,28 @@ class Solution {
         int second = solve(grid,r,c-1,dp);
 
         return dp[r][c] =  Math.min(first,second)+grid[r][c];
+    }
+    */
+
+    //Tabulation -->
+    public static int solve(int [][] grid,int r,int c,int [][] dp){
+        dp[0][0] = grid[0][0];
+        //if(dp[r][c] != -1)return dp[r][c];
+
+        for(int i = 1;i<r;i++){
+            dp[i][0] = dp[i-1][0] + grid[i][0];
+        }
+
+        for(int j = 1;j<c;j++){
+            dp[0][j] = dp[0][j-1] + grid[0][j];
+        }
+
+        for(int i = 1;i<r;i++){
+            for(int j = 1;j<c;j++){
+                dp[i][j] = Math.min(dp[i-1][j],dp[i][j-1])+grid[i][j];
+            }
+        }
+
+        return dp[r-1][c-1];
     }
 }
