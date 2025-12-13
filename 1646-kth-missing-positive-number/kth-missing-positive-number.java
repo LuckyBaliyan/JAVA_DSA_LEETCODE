7535d3bf23b805a3e5c535d07256f2ce246a0045
Lucky Baliyan <baliyanlucky85@gmail.com>
2025-12-13 12:59:12 +0530

    Time: 0 ms (100.00%) | Memory: 44.2 MB (64.83%) - LeetSync

--- 1646-kth-missing-positive-number/kth-missing-positive-number.java
+++ 1646-kth-missing-positive-number/kth-missing-positive-number.java
@@ -1,5 +1,12 @@
 class Solution {
     public int findKthPositive(int[] arr, int k) {
+       // return solveApproach1(arr,k);
+
+       return solveApproach2(arr,k);
+    }
+
+   /*
+    public static int solveApproach1(int [] arr,int k){
        int n = arr.length;
        int h = arr[n-1];
 
@@ -24,5 +31,17 @@ class Solution {
        } 
 
        return -1;
+    }
+    */
+
+    public  static int solveApproach2(int [] arr,int k){
+        int n = arr.length;
+
+        for(int i=0;i<n;i++){
+            if(arr[i] <= k)k++;
+            else break;
+        }
+
+        return k;
     }
 }
