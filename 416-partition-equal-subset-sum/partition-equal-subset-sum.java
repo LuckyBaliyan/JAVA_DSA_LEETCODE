dec4c60f58cadf9b89b6436c059d9243a3b981b7
Lucky Baliyan <baliyanlucky85@gmail.com>
2025-12-15 23:27:32 +0530

    Time: 45 ms (61.34%) | Memory: 47.8 MB (48.15%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -4,8 +4,8 @@ class Solution {
         int sum  = 0;
         for(int num : nums)sum+=num;
         if(sum % 2 == 1)return false;
-        Boolean [][] dp = new Boolean [n][sum/2+1];
-        return solve(nums,n-1,sum/2,dp);
+        boolean [][] dp = new boolean [n][sum/2+1];
+        return solveTab(nums,n,sum/2,dp);
     }
 
   /* plain recursion 
@@ -30,5 +30,32 @@ class Solution {
             return dp[i][sum]  = solve(nums,i-1,sum-nums[i],dp) || solve(nums,i-1,sum,dp);
         }
         else return dp[i][sum] = solve(nums,i-1,sum,dp);
+    }
+
+    public boolean solveTab(int [] nums,int n,int sum,boolean [][] dp){
+        //filling up the first row
+        for(int i = 0;i<n;i++){
+            dp[i][0] = true;
+        }
+
+        //first condition matching case
+        if(nums[0]<= sum){
+            dp[0][nums[0]] = true;
+        }
+
+        for(int i = 1;i<n;i++){
+            for(int s = 1;s<=sum;s++){
+                boolean notTake = dp[i-1][s];
+                boolean take = false;
+
+                if(nums[i] <= s){
+                    take = dp[i-1][s-nums[i]];
+                }
+
+                dp[i][s] = take || notTake;
+            }
+        }
+
+        return dp[n-1][sum];
     }
 }
