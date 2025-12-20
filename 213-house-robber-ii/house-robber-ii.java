bc4794ef0bf672912b8acabda7b79983e591958b
Lucky Baliyan <baliyanlucky85@gmail.com>
2025-12-20 17:02:57 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (26.28%) - LeetSync

---
+++ 213-house-robber-ii/house-robber-ii.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+
+        if(n == 1)return nums[0];
+
+        int [] dp1 = new int [n];
+        Arrays.fill(dp1,-1);
+
+        int [] dp2 = new int [n];
+        Arrays.fill(dp2,-1);
+
+        int case1 = solve(nums,0,n-2,dp1);
+        int case2 = solve(nums,1,n-1,dp2);
+
+        return Math.max(case1,case2);
+    }
+
+    public static int solve(int [] arr,int i,int end,int [] dp){
+        if(i > end)return 0;
+
+        if(dp[i] != -1)return dp[i];
+
+        return dp[i] =  Math.max(arr[i]+solve(arr,i+2,end,dp),solve(arr,i+1,end,dp));
+    }
+}
