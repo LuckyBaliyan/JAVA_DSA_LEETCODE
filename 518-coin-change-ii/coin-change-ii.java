cb453116e4500cf7c48b650ad1841239bbb16974
Lucky Baliyan <baliyanlucky85@gmail.com>
2025-12-22 11:01:23 +0530

    Time: 16 ms (37.31%) | Memory: 76.9 MB (5.44%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -3,7 +3,7 @@ class Solution {
         int n = coins.length;
         int [][] dp = new int [n][amount + 1];
         for(int [] arr: dp)Arrays.fill(arr,-1);
-        return solve(coins,amount,n-1,dp);
+        return solve(coins,amount);
     }
 
 /* Plain Recusrsion
@@ -19,6 +19,8 @@ class Solution {
        return take + notTake; 
     }
 */
+
+/*
     public static int solve(int [] arr,int target,int i,int [][] dp){
        if(target == 0)return 1;
        if(i < 0 || target < 0)return 0;
@@ -32,4 +34,33 @@ class Solution {
        int notTake = solve(arr,target,i-1,dp);
        return dp[i][target] = take + notTake; 
     }
+*/
+    public static int solve(int [] arr,int target){
+       int n = arr.length;
+
+       int [][] dp = new int [n][target+1];
+
+       //Base Case 
+       for(int i = 0;i<n;i++){
+        dp[i][0] = 1;
+       }
+
+       for(int j = 0;j<=target;j++){
+        if(j % arr[0] == 0) dp[0][j] = 1;
+       }
+
+       for(int i = 1;i<n;i++){
+        for(int j = 1;j<=target;j++){
+            int take = 0;
+            if(j>=arr[i]){
+                take = dp[i][j - arr[i]];
+            }
+            int notTake = dp[i-1][j];
+            dp[i][j] = take + notTake;
+        }
+       }
+
+       return dp[n-1][target];  
+    }
+    
 }
