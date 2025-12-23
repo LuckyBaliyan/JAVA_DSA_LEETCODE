b20d13e5c7eed1be9f7ba897271ffa3b1f714aba
Lucky Baliyan <baliyanlucky85@gmail.com>
2025-12-24 00:09:55 +0530

    Time: 38 ms (65.13%) | Memory: 64.3 MB (11.65%) - LeetSync

--- 516-longest-palindromic-subsequence/longest-palindromic-subsequence.java
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.java
@@ -1,9 +1,9 @@
 class Solution {
     public int longestPalindromeSubseq(String s) {
         int n = s.length();
-        int [][] dp = new int [n][n];
-        for(int [] arr:dp)Arrays.fill(arr,-1);
-        return solve(s,0,n-1,dp);
+        //int [][] dp = new int [n][n];
+        //for(int [] arr:dp)Arrays.fill(arr,-1);
+        return solveTab(s);
     }
 
     //Memoization
@@ -25,28 +25,23 @@ class Solution {
 
         int [][] dp = new int [n][n];
 
-        //base cell if first pointers are same i.e 0,0 the letter is same
-        dp[0][0] = 1;
-
-        //base case for all l > r
-        for(int i = 1;i<n;i++){
-            dp[i][0] = 0;
+        //base case for all l == r
+        for(int i = 0;i<n;i++){
+            dp[i][i] = 1;
         }
 
-        for(int j = 1;j<n;j++){
-            if(s.charAt(0) == s.charAt(j))dp[0][j] = 1;
-        }
-
-        for(int i = 1;i<n;i++){
-            for(int j = 1;j<n;j++){
-                if(s.charAt(i) == s.charAt(j)){
-                    dp[i][j] = dp[i+1][j-1]+2;
+        for(int gap = 1; gap < n; gap++){
+            for(int l = 0; l + gap < n; l++){
+                int r = l + gap;
+    
+                if(s.charAt(l) == s.charAt(r)){
+                    dp[l][r] = 2 + (gap == 1 ? 0 : dp[l+1][r-1]);
+                } else {
+                    dp[l][r] = Math.max(dp[l+1][r], dp[l][r-1]);
                 }
-
-                dp[i][j] = Math.max(dp[i+1][j],dp[i][j+1]);
             }
         }
 
-        return dp[n-1][n-1];
+        return dp[0][n-1];
     }
 }
