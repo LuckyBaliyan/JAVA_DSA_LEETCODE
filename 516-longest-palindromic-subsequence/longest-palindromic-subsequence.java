e56e7a237ba72e1ab5570e916d917548259c23ff
Lucky Baliyan <baliyanlucky85@gmail.com>
2025-12-23 23:53:54 +0530

    Time: 23 ms (94.26%) | Memory: 64.2 MB (23.86%) - LeetSync

--- 516-longest-palindromic-subsequence/longest-palindromic-subsequence.java
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.java
@@ -6,6 +6,7 @@ class Solution {
         return solve(s,0,n-1,dp);
     }
 
+    //Memoization
     public static int solve(String s,int l,int r,int [][] dp){
         if(l > r)return 0;
         if(l == r)return 1;
@@ -17,5 +18,35 @@ class Solution {
         }
 
         return dp[l][r] = Math.max(solve(s,l+1,r,dp),solve(s,l,r-1,dp));
+    }
+
+    public static int solveTab(String s){
+        int n = s.length();
+
+        int [][] dp = new int [n][n];
+
+        //base cell if first pointers are same i.e 0,0 the letter is same
+        dp[0][0] = 1;
+
+        //base case for all l > r
+        for(int i = 1;i<n;i++){
+            dp[i][0] = 0;
+        }
+
+        for(int j = 1;j<n;j++){
+            if(s.charAt(0) == s.charAt(j))dp[0][j] = 1;
+        }
+
+        for(int i = 1;i<n;i++){
+            for(int j = 1;j<n;j++){
+                if(s.charAt(i) == s.charAt(j)){
+                    dp[i][j] = dp[i+1][j-1]+2;
+                }
+
+                dp[i][j] = Math.max(dp[i+1][j],dp[i][j+1]);
+            }
+        }
+
+        return dp[n-1][n-1];
     }
 }
