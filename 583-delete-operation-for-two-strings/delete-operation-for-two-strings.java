221fc0d18b67f16a0dbf7681f1023908e49edbab
Lucky Baliyan <baliyanlucky85@gmail.com>
2025-12-26 15:31:37 +0530

    Time: 8 ms (22.90%) | Memory: 46.9 MB (5.17%) - LeetSync

--- 583-delete-operation-for-two-strings/delete-operation-for-two-strings.java
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.java
@@ -21,4 +21,31 @@ class Solution {
 
         return dp[i][j] = Math.min(solve(t1,t2,n1,n2,i+1,j,dp),solve(t1,t2,n1,n2,i,j+1,dp))+1;
     }
+
+    //tabulation
+    public static int solveTab(String t1,String t2){
+        int n1  = t1.length();
+        int n2  = t2.length();
+
+        int [][] dp = new int [n1+1][n2+1];
+
+        //filling up the rows first 
+        for(int i = n1;i>=0;i--){
+            dp[i][n2] = n1 - i;
+        }
+
+        //filling up the columns 
+        for(int j = n2;j>=0;j--){
+            dp[n1][j] = n2 - j;
+        }
+
+        for(int i = n1-1;i>=0;i--){
+            for(int j = n2-1;j>=0;j--){
+                if(t1.charAt(i) == t2.charAt(j)) dp[i][j] = dp[i+1][j+1];
+                else dp[i][j] = Math.min(dp[i+1][j],dp[i][j+1])+1;
+            }
+        }
+
+        return dp[0][0];
+    }
 }
