7c241b0ce40235a0061aacce171b928579a6fb12
Lucky Baliyan <baliyanlucky85@gmail.com>
2025-12-26 18:20:19 +0530

    Time: 23 ms (89.77%) | Memory: 66.6 MB (16.15%) - LeetSync

--- 718-maximum-length-of-repeated-subarray/maximum-length-of-repeated-subarray.java
+++ 718-maximum-length-of-repeated-subarray/maximum-length-of-repeated-subarray.java
@@ -3,7 +3,7 @@ class Solution {
     public int findLength(int[] nums1, int[] nums2) {
         int n = nums1.length;
         int m = nums2.length;
-
+        /*
         int ans = 0;
 
         dp = new int [n+1][m+1];
@@ -16,9 +16,12 @@ class Solution {
         }
 
         return ans;
+        */
+
+        return solve(nums1,nums2);
     }
   
-    //Basic plain recursion for each (i,j) 
+    //Basic plain recursion + Memo for each (i,j) 
     public static int solve(int [] arr1,int [] arr2,int n,int m,int i,int j){
         if(i ==  n || j == m)return 0;
 
@@ -26,5 +29,37 @@ class Solution {
 
         if(arr1[i] == arr2[j])return dp[i][j] = solve(arr1,arr2,n,m,i+1,j+1)+1;
         return dp[i][j] =  0;
+    }
+
+    public static int solve(int [] a,int [] b){
+        int n = a.length;
+        int m = b.length;
+
+        int [][] st = new int[n][m];
+        int maxLen = 0;
+
+        //filling up the same columns from bottom
+        for(int i = 0;i<m;i++){
+           if(a[n - 1] == b[i])st[n-1][i] = 1;
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (a[i] == b[m - 1]) {
+                st[i][m - 1] = 1;
+                maxLen = 1;
+            }
+        }
+
+
+        for(int i = n-2;i>=0;i--){
+            for(int j = m-2;j>=0;j--){
+                if(a[i] == b[j]){
+                    st[i][j] = st[i+1][j+1] + 1;
+                    maxLen = Math.max(maxLen,st[i][j]);
+                }
+            }
+        }
+
+        return maxLen;
     }
 }
