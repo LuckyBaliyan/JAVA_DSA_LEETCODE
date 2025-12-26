42e7a2ffc025862db29afb5b555f806448e31294
Lucky Baliyan <baliyanlucky85@gmail.com>
2025-12-26 21:56:15 +0530

    Time: 6 ms (80.62%) | Memory: 46.1 MB (19.22%) - LeetSync

--- 1105-uncrossed-lines/uncrossed-lines.java
+++ 1105-uncrossed-lines/uncrossed-lines.java
@@ -4,11 +4,13 @@ class Solution {
         int n2 = nums2.length;
 
         // in case of confusion b/w filling dp with -1 ask urself is zero can be an answer or not
-        int [][] dp = new int [n1+1][n2+1];
+        /*int [][] dp = new int [n1+1][n2+1];
         for(int [] arr:dp)Arrays.fill(arr,-1);
 
         return solve(nums1,nums2,n1,n2,0,0,dp);
         //return solve(nums1,nums2,n1,n2,0,0);
+        */
+        return solve(nums1,nums2);
     }
 
     /*
@@ -33,5 +35,21 @@ class Solution {
             solve(a,b,n1,n2,i+1,j,dp),
             solve(a,b,n1,n2,i,j+1,dp)
         );
+    }
+
+    public static int solve(int [] a,int [] b){
+        int n = a.length;
+        int m = b.length;
+
+        int [][] dp = new int [n+1][m+1];
+
+        for(int i = n - 1;i>=0;i--){
+           for(int j = m - 1;j>=0;j--){
+              if(a[i] == b[j]) dp[i][j] = 1 + dp[i+1][j+1];
+              else dp[i][j] = Math.max(dp[i+1][j],dp[i][j+1]);
+           }
+        }
+
+        return dp[0][0];
     }
 }
