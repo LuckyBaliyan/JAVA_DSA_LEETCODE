c3530c9b10f1b0c1de42cfd8a7b0b40d6ae3623d
Lucky Baliyan <baliyanlucky85@gmail.com>
2026-01-03 23:28:55 +0530

    Time: 3 ms (62.90%) | Memory: 43.7 MB (40.02%) - LeetSync

--- 1078-remove-outermost-parentheses/remove-outermost-parentheses.java
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.java
@@ -6,18 +6,18 @@ class Solution {
         int cnt1 = 0;
         int cnt2 = 0;
 
-        String temp = "";
+        StringBuilder temp = new StringBuilder();
 
         for(int i = 0;i<n;i++){
           if(s.charAt(i) == '(')cnt1++;
           else cnt2++;
 
           if(cnt1 == cnt2){
-            temp += s.substring(idx + 2,i);
+            temp.append(s.substring(idx + 2,i));
             idx = i;
           }
         }
 
-        return temp;
+        return temp.toString();
     }
 }
