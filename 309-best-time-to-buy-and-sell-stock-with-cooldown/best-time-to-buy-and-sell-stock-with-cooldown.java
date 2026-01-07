cfdb3a2f6307e077f71b54e7eac35b31514365eb
Lucky Baliyan <baliyanlucky85@gmail.com>
2026-01-07 14:45:05 +0530

    Time: 1 ms (69.48%) | Memory: 43.6 MB (47.46%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -2,11 +2,13 @@ class Solution {
     public int maxProfit(int[] prices) {
         int n = prices.length;
 
-        int [][] dp  = new int [n+1][2];
-        for(int [] arr:dp)Arrays.fill(arr,-1);
+        //int [][] dp  = new int [n+1][2];
+        //for(int [] arr:dp)Arrays.fill(arr,-1);
         //return solve(prices,0,n,1);
 
-        return solveMemo(prices,0,n,1,dp);
+        //return solveMemo(prices,0,n,1,dp);
+
+        return solveTab(prices);
     }
 
     public static int solve(int [] prices,int i,int n,int flag){
@@ -45,5 +47,25 @@ class Solution {
             int skip = solveMemo(prices,i+1,n,0,dp);
             return dp[i][flag] = Math.max(sell,skip);
         }
+    }
+
+    public static int solveTab(int [] prices){
+        int n = prices.length;
+
+        int [][] dp = new int [n+2][2];
+
+        for(int i = n-1; i>=0; i--){
+            int maxProfit = 0;
+            for(int j = 0;j<2;j++){
+               if(j == 1){
+                maxProfit = Math.max(dp[i+1][0]-prices[i],dp[i+1][1]);
+               }
+               else maxProfit = Math.max(dp[i+2][1]+prices[i],dp[i+1][0]);
+
+                dp[i][j] = maxProfit;
+            }
+        }
+
+        return dp[0][1];
     }
 }
