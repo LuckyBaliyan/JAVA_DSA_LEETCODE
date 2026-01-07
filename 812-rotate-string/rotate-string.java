771504117dbe9ced109a96b4b460e6d536090dfc
Lucky Baliyan <baliyanlucky85@gmail.com>
2026-01-07 21:17:57 +0530

    Time: 1 ms (24.60%) | Memory: 43.5 MB (6.01%) - LeetSync

---
+++ 812-rotate-string/rotate-string.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean rotateString(String s, String goal) {
+        int n = s.length();
+        int m = goal.length();
+       //Base case very important if both strings are not equal then we never had 2 equal strings.
+
+       if(n != m)return false;
+
+       if(s.equals(goal))return true;
+
+       for(int i = 0;i<n-1;i++){
+         String rot = s.substring(0,i+1);
+         String rem = s.substring(i+1);
+
+         String res = rem + rot;
+
+         if(res.equals(goal))return true;
+       }
+
+       return false;
+    }
+}
