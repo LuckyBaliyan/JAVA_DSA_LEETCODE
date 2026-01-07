5f5d6e2d3e375c9a08c865a45c28f4797a239fff
Lucky Baliyan <baliyanlucky85@gmail.com>
2026-01-07 23:56:22 +0530

    Time: 93 ms (21.20%) | Memory: 237.5 MB (5.48%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,11 +1,34 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
         int n  = nums.length;
+        //int [][] dp = new int [n+1][n+1];
+
+        //for(int [] arr:dp)Arrays.fill(arr,-1);
+
+        //return solveMemo(nums,0,-1,n,dp);
+        return tab(nums);
+    }
+
+    public static int tab(int [] nums){
+        int n = nums.length;
+
         int [][] dp = new int [n+1][n+1];
 
-        for(int [] arr:dp)Arrays.fill(arr,-1);
+        for(int i = n-1;i>=0;i--){
+            for(int prev = i-1;prev>=-1;prev--){
+                int take = 0;
+                int skip = 0;
+                if(prev == -1 || nums[i] > nums[prev] ){
+                    take = 1 + dp[i+1][i+1];
+                }
+                // we don't have to use else jb max compute kr re 
+                // because hme max ke liye dono values chaiye hr iteration pe!
+                skip = dp[i+1][prev+1];
+                dp[i][prev+1]  = Math.max(skip,take);
+            }
+        }
 
-        return solveMemo(nums,0,-1,n,dp);
+        return dp[0][0];
     }
 
     public static int solveMemo(int [] nums,int i,int prev,int n,int [][] dp){
