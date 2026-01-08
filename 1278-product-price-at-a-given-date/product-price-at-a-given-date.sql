cc5dbdf44575c2ee9b8f213f50c7726b643276c8
Lucky Baliyan <baliyanlucky85@gmail.com>
2026-01-08 14:57:46 +0530

    Time: 497 ms (40.26%) | Memory: 0B (100.00%) - LeetSync

--- 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
+++ 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
@@ -1,17 +1,20 @@
-SELECT product_id, 10 AS price
-FROM Products
-GROUP BY product_id
-HAVING MIN(change_date) > '2019-08-16'
+SELECT product_id, price
+FROM (
+    SELECT 
+        product_id,
+        new_price AS price,
+        DENSE_RANK() OVER (
+            PARTITION BY product_id
+            ORDER BY change_date DESC
+        ) AS rnk
+    FROM Products
+    WHERE change_date <= '2019-08-16'
+) t
+WHERE rnk = 1
 
 UNION ALL
 
-SELECT P.product_id, P.new_price AS price
-FROM Products P
-INNER JOIN (
-    SELECT product_id, MAX(change_date) AS max_date
-    FROM Products
-    WHERE change_date <= '2019-08-16'
-    GROUP BY product_id
-) AS R
-ON P.product_id = R.product_id
-AND P.change_date = R.max_date;
+SELECT product_id, 10 AS price
+FROM Products
+GROUP BY product_id
+HAVING MIN(change_date) > '2019-08-16';
