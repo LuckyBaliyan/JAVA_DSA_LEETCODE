a8ce76caaadfbda16965df654af849284cc09b6c
Lucky Baliyan <baliyanlucky85@gmail.com>
2026-01-09 23:33:45 +0530

    Time: 115 ms (5.31%) | Memory: 64.5 MB (10.47%) - LeetSync

--- 516-longest-palindromic-subsequence/longest-palindromic-subsequence.java
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.java
@@ -1,47 +1,31 @@
 class Solution {
     public int longestPalindromeSubseq(String s) {
         int n = s.length();
-        //int [][] dp = new int [n][n];
-        //for(int [] arr:dp)Arrays.fill(arr,-1);
-        return solveTab(s);
+        int [][] dp = new int [n][n];
+        for(int [] arr:dp)Arrays.fill(arr,-1);
+
+        return solve(s,0,n-1,dp);
     }
 
-    //Memoization
     public static int solve(String s,int l,int r,int [][] dp){
-        if(l > r)return 0;
+        int n = s.length();
+
+        if(l > n || r < 0)return 0;
+        if(l>r)return 0;
+
         if(l == r)return 1;
 
         if(dp[l][r] != -1)return dp[l][r];
 
+        int take = 0;
         if(s.charAt(l) == s.charAt(r)){
-          return dp[l][r] =  solve(s,l+1,r-1,dp)+2;
+            // if b == b so length is 2 not 1 so +2 instead of +1
+            take = solve(s,l+1,r-1,dp)+2;
         }
 
-        return dp[l][r] = Math.max(solve(s,l+1,r,dp),solve(s,l,r-1,dp));
-    }
-
-    public static int solveTab(String s){
-        int n = s.length();
-
-        int [][] dp = new int [n][n];
-
-        //base case for all l == r
-        for(int i = 0;i<n;i++){
-            dp[i][i] = 1;
-        }
-
-        for(int gap = 1; gap < n; gap++){
-            for(int l = 0; l + gap < n; l++){
-                int r = l + gap;
-    
-                if(s.charAt(l) == s.charAt(r)){
-                    dp[l][r] = 2 + (gap == 1 ? 0 : dp[l+1][r-1]);
-                } else {
-                    dp[l][r] = Math.max(dp[l+1][r], dp[l][r-1]);
-                }
-            }
-        }
-
-        return dp[0][n-1];
+        return dp[l][r] = Math.max(
+            take,
+            Math.max(solve(s,l+1,r,dp),solve(s,l,r-1,dp))
+            );
     }
 }
