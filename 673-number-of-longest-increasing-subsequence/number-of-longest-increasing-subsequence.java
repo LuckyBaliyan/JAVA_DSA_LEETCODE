5e0525921fca352d99b1946af9983a66caae37a1
Lucky Baliyan <baliyanlucky85@gmail.com>
2026-01-11 13:59:03 +0530

    Time: 51 ms (5.04%) | Memory: 47 MB (5.66%) - LeetSync

--- 673-number-of-longest-increasing-subsequence/number-of-longest-increasing-subsequence.java
+++ 673-number-of-longest-increasing-subsequence/number-of-longest-increasing-subsequence.java
@@ -6,34 +6,40 @@ class Solution {
     public static int solveTab(int [] arr){
         int n = arr.length;
 
-        int [][] dp = new int [n+1][n+1];
-        int [][] dpCnt = new int [n+1][n+1];
+        int [] curr = new int [n+1];
+        int [] next = new int [n+1];
+        int [] currCnt = new int [n+1];
+        int [] nextCnt = new int [n+1];
+       
 
-        for(int j = 0;j<=n;j++)dpCnt[n][j] = 1;
+        for(int j = 0;j<=n;j++)nextCnt[j] = 1;
 
         for(int i = n-1;i>=0;i--){
             for(int j = -1;j<=i-1;j++){
                 int take = 0,takeCnt = 0;
                 if(j == -1 || arr[i] > arr[j]){
-                    take = 1 + dp[i+1][i+1];
-                    takeCnt = dpCnt[i+1][i+1];
+                    take = 1 + next[i+1];
+                    takeCnt = nextCnt[i+1];
                 }
 
-                int notTake = dp[i+1][j+1];
-                int notTakeCnt = dpCnt[i+1][j+1];
+                int notTake = next[j+1];
+                int notTakeCnt = nextCnt[j+1];
 
-                dp[i][j+1] = Math.max(take, notTake);
+                curr[j+1] = Math.max(take, notTake);
 
                 if(take > notTake){
-                    dpCnt[i][j+1] = takeCnt;
+                    currCnt[j+1] = takeCnt;
                 }
                 else if(take < notTake){
-                    dpCnt[i][j+1] = notTakeCnt;
+                    currCnt[j+1] = notTakeCnt;
                 }
-                else dpCnt[i][j+1] = takeCnt + notTakeCnt;
+                else currCnt[j+1] = takeCnt + notTakeCnt;
             }
+
+            next = curr.clone();
+            nextCnt = currCnt.clone();
         }
 
-        return dpCnt[0][0];
+        return nextCnt[0];
     }
 }
