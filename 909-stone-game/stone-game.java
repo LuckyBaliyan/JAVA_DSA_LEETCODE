1f8f44077024fe7206fe8aff8beccc9348bf5dd8
Lucky Baliyan <baliyanlucky85@gmail.com>
2026-01-12 12:13:57 +0530

    Time: 9 ms (16.37%) | Memory: 46.8 MB (15.75%) - LeetSync

--- 909-stone-game/stone-game.java
+++ 909-stone-game/stone-game.java
@@ -1,5 +1,22 @@
 class Solution {
     public boolean stoneGame(int[] piles) {
-        return true;
+        int n = piles.length;
+        int [][] dp = new int [n][n];
+        for(int [] a:dp)Arrays.fill(a,-1);
+
+        int diff =  solve(piles,0,n-1,dp);
+        return diff > 0;
     }
+
+    public static int solve(int [] arr,int l,int r,int [][] dp){
+        int n = arr.length;
+
+        if(l==r)return arr[l];
+        if(dp[l][r] != -1)return dp[l][r];
+
+        return dp[l][r] = Math.max(
+                          arr[l] - solve(arr,l+1,r,dp),
+                          arr[r] - solve(arr,l,r-1,dp)
+                        );
+    } 
 }
