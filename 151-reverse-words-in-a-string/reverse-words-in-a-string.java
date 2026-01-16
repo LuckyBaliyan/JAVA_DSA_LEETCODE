5340e041c17102deb031d1cd32d93715053ebe95
Lucky Baliyan <baliyanlucky85@gmail.com>
2026-01-16 14:04:13 +0530

    Time: 4 ms (94.24%) | Memory: 44.3 MB (74.01%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -1,37 +1,26 @@
 class Solution {
     public String reverseWords(String s) {
-       
-       //return bruteSOl(s);
-       String [] words = s.trim().split("\\s+");
-       StringBuilder sb = new StringBuilder();
+        int n = s.length();
 
-       for(int i = words.length - 1;i>=0;i--){
-          sb.append(words[i]);
-          if(i > 0)sb.append(" ");
-       }
+        int i = n-1;
+        int j = n-1;
 
-       return sb.toString();
-
-    }
-
-    public static String bruteSOl(String s){
-         int n = s.length();
 
         StringBuilder sb = new StringBuilder();
-        int end = n-1;
 
-        for(int i = n-1;i >= 0;i-- ){
-            char ch = s.charAt(i);
-
-            if(ch == ' '){
-              if( i < end){
-                sb.append(s.substring(i+1,end+1)+" ");
-              }
-              end = i - 1;
+        while(i>=0){
+            if(s.charAt(i) == ' '){
+               if( j > i){
+               sb.append(s.substring(i+1,j+1));
+               sb.append(" ");
+               }
+               j = i - 1;
             }
             else if (i == 0){
-                sb.append(s.substring(i,end+1));
+                sb.append(s.substring(i,j+1));
             }
+
+            i--;
         }
 
         return sb.toString().trim();
