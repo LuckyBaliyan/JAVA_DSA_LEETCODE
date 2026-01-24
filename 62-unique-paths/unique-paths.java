8519eb8a0400bd2d34d7e27ff54e74a5f03c8abf
Lucky Baliyan <baliyanlucky85@gmail.com>
2026-01-24 18:07:35 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (93.83%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -3,12 +3,34 @@ class Solution {
     public int uniquePaths(int m, int n) {
         //return solveRec(m - 1,n - 1);
 
-        memo = new int [m][n];
-        for(int [] arr:memo)Arrays.fill(arr,-1);
+        //memo = new int [m][n];
+        //for(int [] arr:memo)Arrays.fill(arr,-1);
 
-        return solveMemo(m-1,n-1,memo);
+        //return solveMemo(m-1,n-1,memo);
+
+        return solveTab(m,n);
     }
 
+    public static int solveTab(int m,int n){
+        int [][] dp = new int[m][n];
+
+        dp[0][0]  = 1;
+
+        for(int i = 0;i<m;i++){
+            for(int j = 0;j<n;j++){
+                if(i == 0 && j == 0)continue;
+
+                int up = (i > 0)?dp[i-1][j]:0;
+                int down = (j > 0)?dp[i][j-1]:0;
+
+                dp[i][j] = up + down;
+            }
+        }
+
+        return dp[m-1][n-1];
+    }
+
+    // Memoized Solutio --> O(m*n), O(m*n)
     public static int solveMemo(int m,int n,int [][] memo){
         if(m < 0 || n < 0)return 0;
 
