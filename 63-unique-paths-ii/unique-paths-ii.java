ed4d3a80b084ae1a21608541f290673441dc3a2b
Lucky Baliyan <baliyanlucky85@gmail.com>
2026-01-25 22:19:02 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (18.47%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -6,9 +6,34 @@ class Solution {
         int n = obstacleGrid[0].length;
         
         if(obstacleGrid[m-1][n-1] == 1)return 0;
-        
-        dp = new int [m][n];
-        return solveMemo(obstacleGrid,0,0,dp);
+
+        /*dp = new int [m][n];
+        return solveMemo(obstacleGrid,0,0,dp); 
+        --> O(m*n), O(m*n) + O(m+n)
+        */
+
+        return solveTab(obstacleGrid);
+    }
+
+    public int solveTab(int [][] grid){
+        int m = grid.length;
+        int n = grid[0].length;
+
+        if(grid[m-1][n-1] == 1)return 0;
+
+        dp = new int [m+1][n+1];
+
+        dp[m-1][n-1] = 1;
+
+        for(int i = m-1;i>=0;i--){
+            for(int j = n-1;j>=0;j--){
+                if(grid[i][j] == 1)continue;
+                else if (i == m-1 && j == n-1)continue;
+                else dp[i][j] = dp[i+1][j] + dp[i][j+1];
+            }
+        }
+
+        return dp[0][0];
     }
 
     public int solveMemo(int [][] grid,int i,int j,int [][] dp){
