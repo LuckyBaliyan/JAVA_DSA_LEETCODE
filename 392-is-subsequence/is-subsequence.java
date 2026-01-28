bb744142d03e10d7824da0bfee4fb8ea1bfd9155
Lucky Baliyan <baliyanlucky85@gmail.com>
2026-01-28 20:29:59 +0530

    Time: 3 ms (4.90%) | Memory: 45.3 MB (5.21%) - LeetSync

--- 392-is-subsequence/is-subsequence.java
+++ 392-is-subsequence/is-subsequence.java
@@ -7,22 +7,22 @@ class Solution {
 
         for(int [] a:dp)Arrays.fill(a,-1);
 
-        return solveRec(s,t,n1-1,n2-1) == n1?true:false;
+        return solveMemo(s,t,n1-1,n2-1) == n1?true:false;
     }
 
-    public static int solveRec(String s,String t,int i,int j){
+    public static int solveMemo(String s,String t,int i,int j){
 
         if(i<0 || j<0)return 0;
 
         if(dp[i][j] != -1)return dp[i][j];
 
         if(s.charAt(i) == t.charAt(j)){
-            return dp[i][j] = 1 + solveRec(s,t,i-1,j-1);
+            return dp[i][j] = 1 + solveMemo(s,t,i-1,j-1);
         }
 
         return dp[i][j] = Math.max(
-            solveRec(s,t,i-1,j),
-            solveRec(s,t,i,j-1)
+            solveMemo(s,t,i-1,j),
+            solveMemo(s,t,i,j-1)
         );
     }
 }
