b2713bb35bfce4587a2840b192a6c21a6ec0dd4f
Lucky Baliyan <baliyanlucky85@gmail.com>
2026-02-07 20:01:26 +0530

    Time: 100 ms (13.82%) | Memory: 47.9 MB (30.43%) - LeetSync

--- 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.java
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.java
@@ -1,6 +1,6 @@
 class Solution {
     public int minimumDeletions(String s) {
-        int bs = 0;
+        /*int bs = 0;
         int minCost = 0;
 
         for(char ch:s.toCharArray()){
@@ -11,5 +11,23 @@ class Solution {
         }
 
         return minCost;
+        */
+        
+         Stack<Character> st = new Stack<>();
+        int deletions = 0;
+
+        for(char ch : s.toCharArray()){
+            if(ch == 'b'){
+                st.push('b');
+            } else {
+                if(!st.isEmpty() && st.peek() == 'b'){
+                    st.pop();       
+                    deletions++;    
+                } else {
+                    st.push('a');
+                }
+            }
+        }
+        return deletions;
     }
 }
