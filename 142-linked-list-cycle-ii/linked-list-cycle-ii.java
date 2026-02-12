8bf4473acdd52d7303d312d23321b558771c2190
Lucky Baliyan <baliyanlucky85@gmail.com>
2026-02-12 23:13:50 +0530

    Time: 0 ms (100.00%) | Memory: 46.9 MB (26.81%) - LeetSync

---
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.java
@@ -0,0 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while(fast != null && fast.next!=null){
+            fast = fast.next.next;
+            slow = slow.next;
+
+            if(fast == slow){
+                slow = head;
+
+                while(slow != fast){
+                   slow = slow.next;
+                   fast = fast.next; 
+                   // why move fast because it's not ncessary that the start point 
+                   // is where the cycle detected that can be any point but we have
+                   // to find the starting point of cycle
+
+                   // using L = c - x (turotise formula) 
+                }
+
+                return slow;
+            }
+        }
+
+        return null;
+    }
+}
