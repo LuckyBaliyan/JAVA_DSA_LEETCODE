bfe7dc0958db4f0b559b28923a639ac1ffd338f1
Lucky Baliyan <baliyanlucky85@gmail.com>
2026-02-17 22:15:47 +0530

    Time: 65 ms (19.97%) | Memory: 47.7 MB (91.91%) - LeetSync

---
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.java
@@ -0,0 +1,40 @@
+class Solution{
+    static int [] dr = {-1,1,0,0};
+    static int [] dc =  {0,0,-1,1};
+    public int minimumEffortPath(int [][] heights){
+       int n = heights.length;
+       int m = heights[0].length;
+       PriorityQueue<int[]> q = new 
+       PriorityQueue<>(Comparator.comparingInt(a -> a[0]));
+
+       int [][] min = new int [n][m];
+       for(int [] arr:min)Arrays.fill(arr,Integer.MAX_VALUE);
+       min[0][0] = 0;
+
+       q.offer(new int [] {0,0,0});
+
+       while(!q.isEmpty()){
+        int [] curr = q.poll();
+        int effort = curr[0];
+        int r = curr[1];
+        int c = curr[2];
+
+        for(int i = 0;i<4;i++){
+            int nr = r + dr[i];
+            int nc = c + dc[i];
+
+            if(nr < 0 || nc < 0 || nr >= n || nc >= m)continue;
+
+            int diff = Math.abs(heights[r][c] - heights[nr][nc]);
+            int maxEffort = Math.max(effort,diff);
+
+            if(maxEffort < min[nr][nc]){
+              min[nr][nc] = maxEffort;
+              q.offer(new int [] {maxEffort, nr, nc});
+            }
+        }
+       }
+
+       return min[n-1][m-1];
+    }
+}
