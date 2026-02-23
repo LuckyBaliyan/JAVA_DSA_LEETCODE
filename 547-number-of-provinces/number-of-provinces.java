24c72d5d6e03c3b429d3c35add4cef0163037e98
Lucky Baliyan <baliyanlucky85@gmail.com>
2026-02-23 23:26:09 +0530

    Time: 2 ms (47.79%) | Memory: 47.5 MB (45.29%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,26 +1,49 @@
 class Solution {
+    static int provins;
+    static int [] parent,size;
     public int findCircleNum(int[][] isConnected) {
         int n = isConnected.length;
+        provins = n;
 
-        boolean [] visited = new boolean [n];
-        int provis = 0;
-        
-        for(int i =0;i<n;i++){
-            if(!visited[i]){
-                dfs(isConnected,i,visited);
-                provis++;
+        parent = new int [n];
+        size = new int [n];
+
+        for(int i = 0;i<n;i++){
+            parent[i] = i;
+            size[i] = 1;
+        }
+
+        for(int i = 0;i<n;i++){
+            for(int j = 0;j<n;j++){
+                if(isConnected[i][j] == 1){
+                    union(i,j);
+                }
             }
         }
 
-        return provis;
+        return provins;
     }
 
-    public static void dfs(int [][] grid,int i,boolean [] visited){
-        int n = grid.length;
-        visited[i] = true;
+    public static void union(int u,int v){
+      int p_u = find(u);
+      int p_v = find(v);
 
-        for(int j = 0;j<n;j++){
-            if(!visited[j] && grid[i][j] == 1)dfs(grid,j,visited);
-        }
+      if(p_u == p_v)return;
+
+      if(size[p_u] < size[p_v]){
+        parent[p_u] = p_v;
+        size[p_v] += size[p_u];
+      }
+      else{
+        parent[p_v] = p_u;
+        size[p_u] += size[p_v];
+      }
+
+      provins--;
+    }
+
+    public static int find(int x){
+        if(x == parent[x])return x;
+        return parent[x] = find(parent[x]);
     }
 }
