dff02310c4180ea75a2b5a1ac07642b599284b18
Lucky Baliyan <baliyanlucky85@gmail.com>
2026-02-28 21:11:36 +0530

    Time: 25 ms (65.12%) | Memory: 42.4 MB (84.88%) - LeetSync

---
+++ 1800-concatenation-of-consecutive-binary-numbers/concatenation-of-consecutive-binary-numbers.java
@@ -0,0 +1,19 @@
+class Solution {
+    static int MOD = 1000000007;
+
+    public int concatenatedBinary(int n) {
+        long result = 0;
+        int length = 0;
+
+        for (int i = 1; i <= n; i++) {
+
+            if ((i & (i - 1)) == 0) {
+                length++;
+            }
+
+            result = ((result << length) + i) % MOD;
+        }
+
+        return (int) result;
+    }
+}
