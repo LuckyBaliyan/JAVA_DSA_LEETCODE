1217d9cb964dd05e005770c0999eb92d2c1bb48b
Lucky Baliyan <baliyanlucky85@gmail.com>
2026-03-04 18:52:23 +0530

    Time: 2 ms (94.84%) | Memory: 46.9 MB (96.09%) - LeetSync

--- 1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.java
+++ 1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.java
@@ -1,6 +1,44 @@
 class Solution {
     public int numSpecial(int[][] mat) {
-       return approach1(mat);
+        //TC --> O(N * M) * O(N + M) to O(N^3) 
+        //SC --> O(1)
+       //return approach1(mat);
+
+       //TC --> O(N * M) SC --> O(N + M)
+       return approach2(mat);
+    }
+
+    public int approach2(int [][] mat){
+        int m = mat.length;
+        int n = mat[0].length;
+
+        int [] rowCount = new int [m];
+        int [] colCount = new int [n];
+
+        int specials = 0;
+
+        //scan one's and store them
+        for(int i = 0;i<m;i++){
+            for(int j = 0;j<n;j++){
+                if(mat[i][j] == 1){
+                    rowCount[i]++;
+                    colCount[j]++;
+                }
+            }
+        }
+
+        //count after verfiying 
+        for(int i = 0;i<m;i++){
+            for(int j = 0;j<n;j++){
+                if(mat[i][j] == 1){
+                    if(rowCount[i] == 1 && colCount[j] == 1){
+                        specials++;
+                    }
+                }
+            }
+        }
+
+        return specials;
     }
 
     public int approach1(int [][] mat){
