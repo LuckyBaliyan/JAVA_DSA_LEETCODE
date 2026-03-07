6a34c2a9ddf55ee82edb4f230a6a3fc6400b2ea6
Lucky Baliyan <baliyanlucky85@gmail.com>
2026-03-07 21:42:48 +0530

    Time: 64 ms (18.22%) | Memory: 48 MB (28.39%) - LeetSync

---
+++ 2017-minimum-number-of-flips-to-make-the-binary-string-alternating/minimum-number-of-flips-to-make-the-binary-string-alternating.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int minFlips(String s) {
+        int n = s.length();
+
+        s = s+s; //make up the string n + n to cover all type1 rotations
+        
+        StringBuilder s1 = new StringBuilder();    
+        StringBuilder s2 = new StringBuilder();
+
+
+        for(int i = 0;i<2*n;i++){
+            s1.append((i%2 == 0? '0':'1'));
+            s2.append((i%2 == 0? '1':'0'));
+        }
+
+        int flip1 = 0;
+        int flip2 = 0;
+        int res  = Integer.MAX_VALUE;
+
+        int i = 0;
+        int j = 0;
+
+        while(j<2*n){
+            if(s.charAt(j) != s1.charAt(j))flip1++;
+            if(s.charAt(j) != s2.charAt(j))flip2++;
+
+            //when window size exceed n
+            if(j-i+1>n){
+                if(s.charAt(i)!= s1.charAt(i))flip1--;
+                if(s.charAt(i)!= s2.charAt(i))flip2--;
+
+                //finally move window 
+                i++;
+            }
+ 
+            //when equals n i.e done 1 string comparison
+            if(j-i+1 == n)res = Math.min(res,Math.min(flip1,flip2));
+
+            j++;
+        }
+
+        return res;
+    }
+}
