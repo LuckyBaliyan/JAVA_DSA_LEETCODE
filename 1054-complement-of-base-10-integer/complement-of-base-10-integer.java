739a2cd0d68dfee1288cd6a4c50eae672d572c76
Lucky Baliyan <baliyanlucky85@gmail.com>
2026-03-11 22:12:53 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (91.45%) - LeetSync

--- 1054-complement-of-base-10-integer/complement-of-base-10-integer.java
+++ 1054-complement-of-base-10-integer/complement-of-base-10-integer.java
@@ -1,7 +1,27 @@
 class Solution {
     public int bitwiseComplement(int n) {
         // O(n), O(n)
-        return approach1(n);
+        // return approach1(n);
+
+        //O(log n) , O(1)
+        return approach2(n);
+    }
+
+    public static int approach2(int n){
+        //edge case
+        if(n==0)return 1;
+
+        int counter = 0;
+        int res = 0;
+
+        while(n>0){
+           int r = n % 2;
+           res += Math.pow(2,counter) * ((r == 0)?1:0);
+           n = n/2;
+           counter++;
+        }
+
+        return  res;
     }
 
     public static int approach1(int n){
