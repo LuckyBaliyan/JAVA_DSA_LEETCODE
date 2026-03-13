9c1d68f3b2264951ba8390f0a5ba1802cb4ffe17
Lucky Baliyan <baliyanlucky85@gmail.com>
2026-03-14 02:27:11 +0530

    Time: 10 ms (54.10%) | Memory: 47.3 MB (7.70%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -3,9 +3,10 @@ class Solution {
     public int findTargetSumWays(int[] nums, int target) {
         int n = nums.length;
 
+        /*
         dp = new int [n][2001];
-
         for(int [] arr: dp)Arrays.fill(arr,-1);
+        */
 
         if(n == 1){
             if(nums[0] == 0)return 2;
@@ -13,8 +14,36 @@ class Solution {
         }
 
         //return solveRec(nums,target,0,0,n);
+        // return solveMemo(nums,target,0,0,n);
 
-        return solveMemo(nums,target,0,0,n);
+        return solveTab(nums,target);
+    }
+
+    public static int solveTab(int [] nums,int target){
+        int n = nums.length;
+        dp = new int [n+1][2001];
+
+        dp[0][1000] = 1; //1 way for the [0]
+
+        for(int i = 0;i<n;i++){
+            for(int r = -1000;r<1000;r++){
+                if(dp[i][r+1000] != 0){
+                    int ways = dp[i][r+1000];
+
+                    int neg = r - nums[i];
+                    int pos = r + nums[i];
+
+                    if(neg > -1001){
+                        dp[i+1][neg + 1000] += ways;
+                    }
+                    if(pos < 1001){
+                        dp[i+1][pos + 1000] += ways;
+                    }
+                }
+            }
+        }
+
+        return (target < -1000 || target > 1000)?0:dp[n][target + 1000];
     }
 
     public static int solveMemo(int [] arr,int t,int curr,int i,int n){
