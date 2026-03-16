0bb78a94d431aebe8008f170c22662de075375bc
Lucky Baliyan <baliyanlucky85@gmail.com>
2026-03-16 22:13:40 +0530

    Time: 25 ms (0.66%) | Memory: 68 MB (13.98%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,37 +1,26 @@
 class Solution {
+    static int [][] dp;
     public int rob(int[] nums) {
         int n = nums.length;
-        int [] dp = new int [n+1];
-        Arrays.fill(dp,-1);
-        return solveTab(nums,n);
-        //return solveMemo(nums,0,n,dp);
+        int sum = 0;
+        for(int i:nums)sum += i;
+
+        dp = new int [n+1][sum+1];
+
+        for(int [] arr:dp)Arrays.fill(arr,-1);
+
+        return solveRec(nums,0,0,n);
     }
 
-/*
-    public static int solveMemo(int [] nums,int indx,int n,int [] dp){
+    public static int solveRec(int [] h, int i,int s,int n){
+        if(i >= n)return s;
 
-        if(indx >= n)return 0;
+       // if(i == n-2 || i == n-1)return s;
+       if(dp[i][s] != -1)return dp[i][s];
 
-        if(dp[indx] != -1) return dp[indx];
+        int take = solveRec(h,i+2,s + h[i],n);
+        int notTake = solveRec(h,i+1,s,n);
 
-        int first = nums[indx] + solveMemo(nums,indx + 2,n,dp);
-        int second = solveMemo(nums,indx +  1,n,dp);
-
-        dp[indx] = Math.max(first,second); 
-        return dp[indx];
-    }
-*/
-    public static int solveTab(int [] nums,int n){
-        int [] dp = new int [n+1];
-
-        //Base Case
-        dp[n] = 0;
-        dp[n-1] = nums[n-1];
-
-        for(int i = n-2;i>=0;i--){
-            dp[i] = Math.max(dp[i+1],nums[i]+dp[i+2]);
-        }
-
-        return dp[0];
+        return dp[i][s] = Math.max(take,notTake);
     }
 }
