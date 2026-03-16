6a0f3dd8335269b74e636a7c414e6a3acab48725
Lucky Baliyan <baliyanlucky85@gmail.com>
2026-03-17 00:15:16 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (36.47%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -1,43 +1,69 @@
 class Solution {
+    //static int [][][] dp;
+    static int [][] dp;
+    static int sum;
+
     public int rob(int[] nums) {
+        // return approach1(nums); -->O(N * SUM * 2) and O(N*SUM*2) give MLE
+
+        dp = new int [nums.length][2];
+        for(int [] arr:dp)Arrays.fill(arr,-1);
+
+        return approach2(nums,0,nums.length,0);
+    }
+
+    public static int approach2(int [] nums,int i,int n,int flag){
+        if(i>=n)return 0;
+
+        if(i == n-1 && flag == 1)return 0;
+
+        if(dp[i][flag] != -1)return dp[i][flag];
+
+        int notTake = approach2(nums,i+1,n,flag);
+        int take = 0;
+        
+        if(i == 0){
+            take = nums[i] + approach2(nums,i+2,n,1);
+        }
+        else take = nums[i] + approach2(nums,i+2,n,flag);
+
+        return dp[i][flag] =  Math.max(take,notTake);
+    }
+
+  /*
+    public static int approach1(int [] nums){
         int n = nums.length;
 
-        if(n == 1)return nums[0];
+        for(int i = 0;i<n;i++)sum += nums[i];
 
-        int [] dp1 = new int [n];
-        Arrays.fill(dp1,-1);
+        dp = new int [n][sum+1][2];
+        for(int i = 0;i<n;i++){
+            for(int j = 0;j<sum;j++){
+                for(int k = 0;k<2;k++)dp[i][j][k] = -1;
+            }
+        }
 
-        int [] dp2 = new int [n];
-        Arrays.fill(dp2,-1);
-
-        //int case1 = solve(nums,0,n-2,dp1);
-        //int case2 = solve(nums,1,n-1,dp2);
-
-        //return Math.max(case1,case2);
-
-        int c1 = solve(nums,n-2,0);
-        int c2 = solve(nums,n-1,1);
-
-        return Math.max(c1,c2);
+        return solveRec(nums,0,0,n,0);
     }
-    /*
-    public static int solve(int [] arr,int i,int end,int [] dp){
-        if(i > end)return 0;
 
-        if(dp[i] != -1)return dp[i];
+    public static int solveRec(int [] arr,int i,int s,
+    int n,int flag){
+        if(i>=n)return s;
+    
+        if(i == n-1 && flag == 1)return s;
 
-        return dp[i] =  Math.max(arr[i]+solve(arr,i+2,end,dp),solve(arr,i+1,end,dp));
+        if(dp[i][s][flag] != -1)return dp[i][s][flag];
+
+        int notTake = solveRec(arr,i+1,s,n,flag);
+        int take = 0;
+        if(i == 0 && flag == 0){
+          take = solveRec(arr,i+2,s + arr[i],n,1);
+        }
+        else{
+            take = solveRec(arr,i+2,s+arr[i],n,flag);
+        }
+
+        return dp[i][s][flag] =  Math.max(notTake,take);
     }
     */
-
-    public static int solve(int [] arr,int end,int start){
-       int n = arr.length;
-       int [] dp = new int [n+2];
-
-       for(int i = end;i>=start;i--){
-        dp[i] = Math.max(dp[i+1],arr[i]+dp[i+2]);
-       }
-
-       return dp[start];
-    }
 }
