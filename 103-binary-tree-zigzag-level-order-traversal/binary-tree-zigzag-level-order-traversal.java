2b86291674f67d6877547da8979cf2717e43353a
Lucky Baliyan <baliyanlucky85@gmail.com>
2026-03-18 09:43:08 +0530

    Time: 1 ms (77.84%) | Memory: 43.6 MB (87.81%) - LeetSync

---
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    static List<List<Integer>> res;
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        res = new ArrayList<>();
+        solveLvl(root, 0);
+
+        return res;
+    }
+
+    public static void solveLvl(TreeNode root,int level){
+        if(root == null)return;
+
+        if(res.size() == level)res.add(new ArrayList<>());
+
+        boolean flag = (level % 2 == 0);
+
+        if(flag){
+           res.get(level).add(root.val);
+        }
+        else{
+            res.get(level).add(0,root.val);
+        }
+
+
+        solveLvl(root.left, level + 1);
+        solveLvl(root.right,level + 1);
+    }
+}
