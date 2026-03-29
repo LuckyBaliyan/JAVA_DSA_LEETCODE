09f7cf0ed01a2c18ae295cd2762354bd320d56e1
Lucky Baliyan <baliyanlucky85@gmail.com>
2026-03-29 19:54:20 +0530

    Time: 2 ms (99.60%) | Memory: 72.6 MB (40.74%) - LeetSync

---
+++ 890-lemonade-change/lemonade-change.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        int five = 0;
+        int ten = 0;
+
+        for(int bill : bills){
+            if(bill == 5)five++;
+
+            if(bill == 10){
+                if(five == 0)return false; //can't give change
+                five--; //reduce 5 as given back to cutomer
+                ten++; //and we have 10 as our earning
+            }
+
+            if(bill == 20){
+                if(five > 0 && ten > 0){
+                    five--;
+                    ten--;
+                }
+                else if (five >= 3)five -= 3;
+                else return false;
+            }
+        }
+
+        return true;
+    }
+}
