6ca221397f146b2319726a76a1d224f4113590eb
Lucky Baliyan <baliyanlucky85@gmail.com>
2026-04-04 19:30:12 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (54.96%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,51 +1,18 @@
 class Solution {
-    public static int [][] memo;
+    static int [][] memo;
     public int uniquePaths(int m, int n) {
-        //return solveRec(m - 1,n - 1);
-
-        //memo = new int [m][n];
-        //for(int [] arr:memo)Arrays.fill(arr,-1);
-
-        //return solveMemo(m-1,n-1,memo);
-
-        return solveTab(m,n);
+        memo = new int [n][m];
+        
+        for(int [] arr:memo)Arrays.fill(arr,-1);
+        return solveRec(n - 1,m - 1);
     }
 
-    public static int solveTab(int m,int n){
-        int [][] dp = new int[m][n];
+    public static int solveRec(int n, int m){
+        if(n < 0 || m < 0)return 0;
+        if(n == 0 && m == 0)return 1;
 
-        dp[0][0]  = 1;
+        if(memo[n][m] != -1)return memo[n][m];
 
-        for(int i = 0;i<m;i++){
-            for(int j = 0;j<n;j++){
-                if(i == 0 && j == 0)continue;
-
-                int up = (i > 0)?dp[i-1][j]:0;
-                int down = (j > 0)?dp[i][j-1]:0;
-
-                dp[i][j] = up + down;
-            }
-        }
-
-        return dp[m-1][n-1];
-    }
-
-    // Memoized Solutio --> O(m*n), O(m*n)
-    public static int solveMemo(int m,int n,int [][] memo){
-        if(m < 0 || n < 0)return 0;
-
-        if(m==0 && n == 0)return 1;
-
-        if(memo[m][n] != -1)return memo[m][n];
-
-        return memo[m][n] = solveMemo(m-1,n,memo) + solveMemo(m,n-1,memo);
-    }
-
-    // Recursive solution with --> O(2^(m+n)) , O(2N)
-    public static int solveRec(int top,int left){
-        if(top < 0 || left < 0)return 0;
-        if(top == 0 && left == 0)return 1;
-
-        return solveRec(top - 1,left) +  solveRec(top,left - 1);
+        return memo[n][m] = solveRec(n-1,m) + solveRec(n,m-1);
     }
 }
