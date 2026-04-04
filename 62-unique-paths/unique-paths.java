f640608b17335ddc1e6eca0636106741797d2a60
Lucky Baliyan <baliyanlucky85@gmail.com>
2026-04-04 19:42:29 +0530

    Time: 1 ms (13.63%) | Memory: 42.3 MB (36.29%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -3,8 +3,31 @@ class Solution {
     public int uniquePaths(int m, int n) {
         memo = new int [n][m];
         
-        for(int [] arr:memo)Arrays.fill(arr,-1);
-        return solveRec(n - 1,m - 1);
+        /*for(int [] arr:memo)Arrays.fill(arr,-1);
+        return solveRec(n - 1,m - 1);*/
+
+        return solveTab(n,m);
+    }
+
+    public static int solveTab(int n, int m){
+        int [][] dp = new int [n][m];
+
+        //base case:- 
+        dp[0][0] = 1;
+
+        for(int i = 0;i<n;i++){
+            for(int j = 0;j<m;j++){
+               if(i == 0 && j == 0)continue;
+               int up = 0;
+               int down = 0;
+               if(i > 0)up = dp[i-1][j];
+               if(j > 0)down = dp[i][j-1];
+
+               dp[i][j] = up + down;
+            }
+        }
+
+        return dp[n-1][m-1];
     }
 
     public static int solveRec(int n, int m){
