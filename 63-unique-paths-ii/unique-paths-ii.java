22fe46ad7803f898fd9ee4d24776b8d41dd1e163
Lucky Baliyan <baliyanlucky85@gmail.com>
2026-04-04 20:23:15 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (54.49%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -4,11 +4,36 @@ class Solution {
         int n = obstacleGrid.length;
         int m = obstacleGrid[0].length;
 
-        memo = new int [n][m];
+        /*memo = new int [n][m];
 
-        for(int [] arr:memo)Arrays.fill(arr,-1);
+        for(int [] arr:memo)Arrays.fill(arr,-1);*/
 
-        return solveRec(obstacleGrid,n-1,m-1);
+        ///return solveRec(obstacleGrid,n-1,m-1);
+
+        return solveTab(obstacleGrid,n,m);
+    }
+
+    public static int solveTab(int [][] arr, int n,int m){
+        int [] dp = new int [m];
+
+        if(arr[0][0] == 1)return 0;
+        if(arr[n-1][m-1] == 1)return 0;
+
+        dp[0] = 1;
+        for(int j = 1; j < m; j++){
+            if(arr[0][j] == 1) dp[j] = 0;
+            else dp[j] = dp[j-1];
+        }
+
+        for(int i = 1;i<n;i++){
+            if(arr[i][0] == 1) dp[0] = 0;
+            for(int j = 1;j<m;j++){
+                if(arr[i][j] == 1)dp[j] = 0;
+                else dp[j] = dp[j] + dp[j-1];
+            }
+        }
+
+        return dp[m-1];
     }
 
     public static int solveRec(int [][] arr,int n, int m){
