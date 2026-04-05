9224c457e51cd29a80f4199a6c5aba24c109df47
Lucky Baliyan <baliyanlucky85@gmail.com>
2026-04-05 14:59:46 +0530

    Time: 5 ms (68.18%) | Memory: 46.4 MB (15.03%) - LeetSync

--- 657-robot-return-to-origin/robot-return-to-origin.java
+++ 657-robot-return-to-origin/robot-return-to-origin.java
@@ -2,9 +2,11 @@ class Solution {
     public boolean judgeCircle(String moves) {
         int n = moves.length();
 
+        //just old damm class 10 concept find total displacement if 0 we are at (0,0)
         int displacementX = 0;
         int displacementY = 0;
 
+        //Im marking U & L as +ve magnitudes of their axises
         for(char c:moves.toCharArray()){
             if(c == 'U')displacementY++;
             else if(c == 'D')displacementY--;
@@ -13,6 +15,8 @@ class Solution {
             else if(c == 'R')displacementX--;
         }
 
+        //at the end check if both axis have 0 displacment so they are at their initial
+        //given position A.T.Q (0,0)
         return displacementY  == 0 && displacementX == 0;
     }
 }
