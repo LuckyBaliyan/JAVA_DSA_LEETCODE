5dd584298849a8bda0407c0c901b9baf1366ce2f
Lucky Baliyan <baliyanlucky85@gmail.com>
2026-04-09 10:22:13 +0530

    Time: 1 ms (99.82%) | Memory: 50.4 MB (27.89%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -1,48 +1,25 @@
 class Solution {
+    static int [][] dp;
     public int minPathSum(int[][] grid) {
-        int r = grid.length;
-        int c = grid[0].length;
+        int n = grid.length;
+        int m = grid[0].length;
 
-        int [][] dp = new int [r][c];
+        dp  = new int [n][m];
 
-        for(int [] arr:dp)Arrays.fill(arr,-1);
-        return solve(grid,r,c,dp);
+        for(int [] arr: dp)Arrays.fill(arr,-1);
+
+        return solve(grid,n-1,m-1);
     }
 
-    // Memoization Approach --->
-    /*
-    public static int solve(int [][] grid,int r,int c,int [][] dp){
-        if(r < 0 || c < 0)return Integer.MAX_VALUE;
-        if(r == 0 && c == 0)return grid[0][0];
+    public int solve(int [][] arr, int i, int j){
+        if(i < 0 || j < 0)return Integer.MAX_VALUE;
+        if(i == 0 && j == 0)return arr[i][j];
 
-        if(dp[r][c] != -1)return dp[r][c];
+        if(dp[i][j] != -1)return dp[i][j];
 
-        int first = solve(grid,r-1,c,dp);
-        int second = solve(grid,r,c-1,dp);
+        int up = solve(arr, i-1, j);
+        int left = solve(arr, i, j-1);
 
-        return dp[r][c] =  Math.min(first,second)+grid[r][c];
-    }
-    */
-
-    //Tabulation -->
-    public static int solve(int [][] grid,int r,int c,int [][] dp){
-        dp[0][0] = grid[0][0];
-        //if(dp[r][c] != -1)return dp[r][c];
-
-        for(int i = 1;i<r;i++){
-            dp[i][0] = dp[i-1][0] + grid[i][0];
-        }
-
-        for(int j = 1;j<c;j++){
-            dp[0][j] = dp[0][j-1] + grid[0][j];
-        }
-
-        for(int i = 1;i<r;i++){
-            for(int j = 1;j<c;j++){
-                dp[i][j] = Math.min(dp[i-1][j],dp[i][j-1])+grid[i][j];
-            }
-        }
-
-        return dp[r-1][c-1];
+        return dp[i][j] =  arr[i][j] + Math.min(up, left);
     }
 }
