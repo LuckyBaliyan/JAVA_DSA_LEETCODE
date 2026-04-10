e5f8ff38a2f22f2dbec35ca330d68f201e677342
Lucky Baliyan <baliyanlucky85@gmail.com>
2026-04-10 12:37:03 +0530

    Time: 4 ms (68.49%) | Memory: 45.2 MB (14.47%) - LeetSync

--- 4115-minimum-distance-between-three-equal-elements-i/minimum-distance-between-three-equal-elements-i.java
+++ 4115-minimum-distance-between-three-equal-elements-i/minimum-distance-between-three-equal-elements-i.java
@@ -3,14 +3,26 @@ class Solution {
         int n = nums.length;
         int ans = Integer.MAX_VALUE;
 
-        for(int i = 0;i <n;i++){
-            for(int j = i+1;j<n;j++){
-                for(int k = j+1;k<n;k++){
-                    if((nums[i] == nums[j]) && (nums[j] == nums[k])){
-                       int sum = Math.abs(i-j) + Math.abs(j - k) + Math.abs(i - k);
-                       ans = Math.min(ans,sum);
-                    }
-                }
+        HashMap<Integer, ArrayList<Integer>> map = new HashMap<>();
+
+        //for each num store it's accourance index wise in a list
+        for(int i = 0;i<n;i++){
+            map.computeIfAbsent(nums[i],k->new ArrayList<>()).add(i);
+        }
+
+        for(ArrayList<Integer> list : map.values()){
+            int size = list.size();
+            //if triplet was there
+            if(size < 3)continue;
+
+            //moving window of 3 like there is a probability of same values 
+            // but more than 3 ex:- [1,1,1,1,1,1,1] here we will check each window 
+            // of 3
+            for(int i = 0;i<=size-3;i++){
+                int a = list.get(i);
+                int k = list.get(i+2);
+
+                ans = Math.min(ans, 2*(k-a));
             }
         }
 
