963cdfea1a3d37f0f70de13165f45b1e86d271ed
Lucky Baliyan <baliyanlucky85@gmail.com>
2026-04-15 14:54:26 +0530

    Time: 11 ms (8.60%) | Memory: 48.6 MB (57.50%) - LeetSync

--- 338-counting-bits/counting-bits.java
+++ 338-counting-bits/counting-bits.java
@@ -1,14 +1,20 @@
 class Solution {
     public int[] countBits(int n) {
-        int [] arr = new int [n+1];
+        int [] ans = new int [n + 1];
 
-        arr[0] = 0;
-        for(int i=1;i<=n;i++){
-            int half = i/2;
-            int last = i%2;
-            arr[i] = arr[half] + last;
-        }
+        for(int i = 0; i<= n; i++){
+            ans[i] = solve(i);
+        } 
 
-        return arr;
+        return ans;
+    }
+
+    public static int solve(int num){
+        //BaseCase required one's to make 0 or 1
+        if(num == 0)return 0;
+        if(num == 1)return 1; 
+
+        if(num % 2 == 0)return solve(num / 2);
+        else return 1 + solve(num / 2);
     }
 }
