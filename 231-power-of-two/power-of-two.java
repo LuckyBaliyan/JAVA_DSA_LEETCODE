f6eb50644850097e1bf0c239350d29129abbeeb3
Lucky Baliyan <baliyanlucky85@gmail.com>
2026-04-15 15:34:42 +0530

    Time: 1 ms (96.53%) | Memory: 42.5 MB (77.97%) - LeetSync

--- 231-power-of-two/power-of-two.java
+++ 231-power-of-two/power-of-two.java
@@ -1,11 +1,19 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        if(n <= 0)return false;
+        /*if(n <= 0)return false;
 
         while(n%2 == 0){
             n = n / 2;
         }
 
-        return n == 1;
+        return n == 1;*/
+
+         // all pow of 2 has only 1 set bit from behind i.e 0000010
+        //Bit manupulation mask of n & (n - 1)
+       //(n - 1) alternate the digits till last set bits 
+
+       //ex- 00001000 AND 000000 == 0
+       // ex-2 00001100 AND 00001011 == 000010000 so not
+       return (n > 0 && ((n&(n - 1)) == 0));
     }
 }
