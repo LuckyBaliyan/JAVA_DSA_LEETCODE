c529332cb6a5a29eae7af32c4f786f14778606ac
Lucky Baliyan <baliyanlucky85@gmail.com>
2026-04-17 11:47:54 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (8.04%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -2,45 +2,20 @@ class Solution {
     static int [] dp;
     public int rob(int[] nums) {
         int n = nums.length;
-        int sum = 0;
-        for(int i:nums)sum += i;
-
-        /*dp = new int [n+1][sum+1];
-
-        for(int [] arr:dp)Arrays.fill(arr,-1);
-
-        return solveRec(nums,0,0,n);*/
-
-        return solveTab(nums);
-    }
-
-    public static int solveTab(int [] nums){
-        int n = nums.length;
-
         dp = new int [n];
-        //base case and i'm standing at 0 
-        dp[0] = nums[0];
+        Arrays.fill(dp, -1);
 
-        for(int i = 1;i<n;i++){
-            int prof1 = dp[i-1]; //if we don't rob curr house 
-            int prof2 = (i-2 < 0)?nums[i]:dp[i-2] + nums[i];
-
-            dp[i] = Math.max(prof1,prof2);
-        }
-
-        return dp[n-1];
+        return solve(nums,0,n);
     }
 
-    /*public static int solveRec(int [] h, int i,int s,int n){
-        if(i >= n)return s;
+    public static int solve(int [] arr, int i, int n){
+        if(i >= n)return 0; 
 
-       // if(i == n-2 || i == n-1)return s;
-       if(dp[i][s] != -1)return dp[i][s];
+        if(dp[i] != -1)return dp[i];
 
-        int take = solveRec(h,i+2,s + h[i],n);
-        int notTake = solveRec(h,i+1,s,n);
+        int skip = solve(arr, i+1, n);
+        int take = arr[i] + solve(arr, i+2, n);
 
-        return dp[i][s] = Math.max(take,notTake);
+        return dp[i] =  Math.max(skip, take);
     }
-    */
 }
