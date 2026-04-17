b323dbf3bb8c30685f1b2da65c3d7f0e892b2eac
Lucky Baliyan <baliyanlucky85@gmail.com>
2026-04-18 01:15:13 +0530

    Time: 1 ms (100.00%) | Memory: 43.3 MB (91.47%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.java
+++ 10-regular-expression-matching/regular-expression-matching.java
@@ -1,13 +1,20 @@
 class Solution {
+    static int [][] dp;
     public boolean isMatch(String s, String p) {
-        return solve(0, 0, s, p);
+        dp = new int [s.length() + 1][p.length() + 1];
+
+        for(int [] d: dp)Arrays.fill(d, -1);
+        int res =  solve(0, 0, s, p);
+        return res == 1 ? true : false;
     }
 
-    public static boolean solve(int i, int j, String s, String p){
+    public static int solve(int i, int j, String s, String p){
         if(j == p.length()){
-            if(i == s.length())return true;
-            return false;
+            if(i == s.length())return 1;
+            return 0;
         }
+
+        if(dp[i][j] != -1)return  dp[i][j];
 
         boolean matched = false;
 
@@ -16,13 +23,13 @@ class Solution {
         matched = true;
 
         if(j + 1 < p.length() && p.charAt(j + 1) == '*'){
-            boolean take  = matched && solve(i+1, j, s, p);
-            boolean skip = solve(i, j+2, s, p); 
+            int take  = matched && (solve(i+1, j, s, p) == 1) ? 1 : 0;
+            int skip = solve(i, j+2, s, p) == 1? 1 : 0; 
             //dont take .*
 
-            return take || skip;
+            return dp[i][j] = (take + skip == 0) ? 0 : 1;
         }
 
-        return matched && solve(i+1, j+1, s, p);
+        return dp[i][j] =  (matched && (solve(i+1, j+1, s, p) == 1))?1: 0;
     }
 }
