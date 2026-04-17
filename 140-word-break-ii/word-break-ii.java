a12f791201fc891eb4322aa48e84301ce6cf5e37
Lucky Baliyan <baliyanlucky85@gmail.com>
2026-04-17 19:04:37 +0530

    Time: 3 ms (53.85%) | Memory: 48.8 MB (26.53%) - LeetSync

--- 140-word-break-ii/word-break-ii.java
+++ 140-word-break-ii/word-break-ii.java
@@ -1,12 +1,19 @@
 class Solution {
     public List<String> wordBreak(String s, List<String> wordDict) {
         HashSet<String> st = new HashSet<>(wordDict);
+        //use a map to avoid rebuilding of same string again from same i
+        HashMap<Integer, List<String>> map = new HashMap<>();
+
         int n = s.length();
 
-        return solve(s,0,n,st);
+        return solve(s,0,n,st,map);
     }
 
-    public List<String> solve(String s, int i, int n, HashSet<String> st){
+    public List<String> solve(String s, int i, int n, HashSet<String> st,
+    HashMap<Integer, List<String>> dp){
+        //return already computed String
+        if(dp.containsKey(i))return dp.get(i);
+
         List<String> res = new ArrayList<>();
 
         if(i == n){
@@ -18,7 +25,7 @@ class Solution {
         for(int j = i; j<n; j++){
            String prefix = s.substring(i, j+1);
            if(st.contains(prefix)){
-                List<String> temp = solve(s, j+1, n, st);
+                List<String> temp = solve(s, j+1, n, st, dp);
     
                 for(String str: temp){
                     if(str.equals("")){
@@ -30,7 +37,8 @@ class Solution {
                 }
            }
         }
-
+        
+        dp.put(i, res);
         return res;
     }
 }
