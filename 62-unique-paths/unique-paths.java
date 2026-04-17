0e5569ffb138cdfef656557ec4857aa32456435c
Lucky Baliyan <baliyanlucky85@gmail.com>
2026-04-17 12:23:40 +0530

    Time: 1 ms (13.68%) | Memory: 42.1 MB (68.87%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,54 +1,21 @@
 class Solution {
-    static int [][] memo;
     public int uniquePaths(int m, int n) {
-        memo = new int [n][m];
-        
-        /*for(int [] arr:memo)Arrays.fill(arr,-1);
-        return solveRec(n - 1,m - 1);*/
+        int [][] dp = new int [m][n];
 
-        return solveTab(n,m);
-    }
+       dp[0][0] = 1;
 
-    public static int solveTab(int n, int m){
-        /*int [][] dp = new int [n][m];
+        for(int i = 0; i<m; i++){
+            for(int j = 0; j<n; j++){
 
-        //base case:- 
-        dp[0][0] = 1;
+                if( i == 0 && j == 0)continue;
+                
+                int up = (i - 1 < 0)? 0 : dp[i-1][j];
+                int left = (j - 1 < 0)? 0: dp[i][j-1];
 
-        for(int i = 0;i<n;i++){
-            for(int j = 0;j<m;j++){
-               if(i == 0 && j == 0)continue;
-               int up = 0;
-               int down = 0;
-               if(i > 0)up = dp[i-1][j];
-               if(j > 0)down = dp[i][j-1];
-
-               dp[i][j] = up + down;
-            }
-        }
-        return dp[n-1][m-1];
-        */
-
-        int [] dp = new int [m];
-
-        //fill each column with 1 for row(0)
-        for(int i = 0;i<m;i++)dp[i] = 1;
-
-        for(int i = 1;i<n;i++){
-            for(int j = 1;j<m;j++){
-                dp[j] = dp[j] + dp[j-1];
+                dp[i][j] = up + left;
             }
         }
 
-        return dp[m-1];
-    }
-
-    public static int solveRec(int n, int m){
-        if(n < 0 || m < 0)return 0;
-        if(n == 0 && m == 0)return 1;
-
-        if(memo[n][m] != -1)return memo[n][m];
-
-        return memo[n][m] = solveRec(n-1,m) + solveRec(n,m-1);
+        return dp[m-1][n-1];
     }
 }
