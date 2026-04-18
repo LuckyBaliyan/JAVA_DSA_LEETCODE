fc84e46a0bed969d0d5835e316b6e60b15041a95
Lucky Baliyan <baliyanlucky85@gmail.com>
2026-04-18 16:54:47 +0530

    Time: 91 ms (5.91%) | Memory: 47 MB (7.44%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -1,52 +1,24 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-      // return slidingWindow(s); --> O(N),O(N)
-
-        return solveSet(s);
-
-    }
-
-    public static int solveSet(String s){
         int n = s.length();
-        HashSet<Character> st = new HashSet<>();
+        int maxLen = 0;
 
-        int left = 0;
-        int right = 0;
-        int max = 0;
+        if(n == 1)return 1;
 
-        while(right < n){
-            char ch = s.charAt(right);
-            while(st.contains(ch)){
-              st.remove(s.charAt(left)); // must delete the previous character 
-              left++;
+        for(int i = 0; i<n; i++){
+            HashSet<Character> st = new HashSet<>();
+            for(int j = i; j<n; j++){
+                char ch = s.charAt(j);
+                
+                if (st.contains(ch)){
+                   break; //never add that substring
+                }
+
+                st.add(ch);
+                maxLen = Math.max(maxLen, st.size());
             }
-
-            st.add(ch);
-            max = Math.max(max,(right - left)+1);
-            right++;
         }
 
-        return max;
-    }
-
-    public static int slidingWindow(String s){
-        int n = s.length();
-        int max = 0;
-
-        StringBuilder sb = new StringBuilder();
-        for(int i = 0;i<n;i++){
-            char ch = s.charAt(i);
-
-            int idx = sb.indexOf(String.valueOf(ch));
-
-            if(idx != -1){
-                sb.delete(0,idx+1);
-            }
-
-            sb.append(ch);
-            max = Math.max(max,sb.length());
-        }
-
-        return max;
+        return maxLen;
     }
 }
