763251beb558aefc0e6ae529d61c4da4488f1ae6
Lucky Baliyan <baliyanlucky85@gmail.com>
2026-04-20 17:44:53 +0530

    Time: 13 ms (39.34%) | Memory: 46.3 MB (63.36%) - LeetSync

--- 794-swim-in-rising-water/swim-in-rising-water.java
+++ 794-swim-in-rising-water/swim-in-rising-water.java
@@ -1,54 +1,44 @@
 class Solution {
-    static int [] dr = {-1,1,0,0};
-    static int [] dc = {0,0,-1,1}; 
-    static class Pair{
-        int cost;
-        int r;
-        int c;
+    static int [] dr = {-1,0,1,0};
+    static int [] dc = {0, -1, 0, 1};
 
-        Pair(int cost,int r,int c){
-            this.cost = cost;
-            this.r = r;
-            this.c = c;
-        }
-    }
     public int swimInWater(int[][] grid) {
-        int n = grid.length;
+        int n  = grid.length;
         int m = grid[0].length;
 
-        PriorityQueue<Pair> pq = 
-        new PriorityQueue<>((a,b)->Integer.compare(a.cost,b.cost));
+        PriorityQueue<int []> pq = 
+        new PriorityQueue<>(Comparator.comparingInt(a->a[0]));
 
-        int [][] time = new int [n][n];
-        for(int [] arr:time)Arrays.fill(arr,Integer.MAX_VALUE);
+        int [][] path = new int [n][m];
+        for(int [] p: path)Arrays.fill(p, Integer.MAX_VALUE);
 
-       // not necessarly to start with zero time[0][0] = grid[0][0];
-       time[0][0] = grid[0][0];
-        pq.offer(new Pair(grid[0][0],0,0));
+        //addd source cell
+        path[0][0] = grid[0][0];
+        pq.offer(new int [] {grid[0][0],0,0});
 
         while(!pq.isEmpty()){
-            Pair p = pq.poll();
-            int cost = p.cost;
-            int r = p.r;
-            int c = p.c;
+            int [] curr = pq.poll();
+            int time = curr[0];
+            int  r = curr[1];
+            int  c = curr[2];
 
-            if(cost > time[r][c])continue;
-            if(r == n-1 && c == m-1)return cost;
+            if(time > path[r][c])continue;
+            if(r == n-1 && c == m-1)return time;
 
-            for(int i = 0;i<4;i++){
+            for(int i = 0; i<4; i++){
                 int nr = r + dr[i];
                 int nc = c + dc[i];
 
-                if(nr < 0 || nr >= n || nc < 0 || nc >= m)continue;
-                int newCost = Math.max(cost, grid[nr][nc]);
+                if(nr < 0 || nr >=n || nc < 0 || nc >=m)continue;
                 
-                if(newCost < time[nr][nc]){
-                    pq.offer(new Pair(newCost,nr,nc));
-                    time[nr][nc] = newCost;
+                int maxTime = Math.max(time, grid[nr][nc]);
+                if(maxTime < path[nr][nc]){
+                    path[nr][nc] = maxTime;
+                    pq.offer(new int [] {maxTime, nr, nc});
                 }
             }
         }
-        
-        return time[n-1][n-1];
+
+        return path[n-1][m-1];
     }
 }
