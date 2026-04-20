e14e40a97ba04c1b0fe78954d80124fe0c25ff60
Lucky Baliyan <baliyanlucky85@gmail.com>
2026-04-20 15:21:11 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (80.83%) - LeetSync

--- 2199-two-furthest-houses-with-different-colors/two-furthest-houses-with-different-colors.java
+++ 2199-two-furthest-houses-with-different-colors/two-furthest-houses-with-different-colors.java
@@ -3,14 +3,31 @@ class Solution {
         int n = colors.length;
         int maxDist = 0;
 
-        for(int i = 0; i<n-1; i++){
-            for(int j = i + 1; j<n; j++){
-                if(colors[i] != colors[j]){
-                    maxDist = Math.max(maxDist, Math.abs(j - i));
-                }
-            }
+        int [] pair1 = new int [] {colors[0], 0}; //pick first from  left
+        int [] pair2 = new int [2]; //pick different from right
+
+        for(int i = n - 1; i>=0; i--){
+           if(pair1[0] != colors[i]){
+             pair2[0] = colors[i];
+             pair2[1] = i;
+             break;
+           }
         }
 
+        maxDist = Math.max(maxDist, Math.abs(pair1[1] - pair2[1]));
+
+        int [] pair3 = {colors[n-1], n-1};
+        int [] pair4 = new int [2];
+
+        for(int i = 0; i<n; i++){
+            if(pair3[0] != colors[i]){
+                pair4[0] = colors[i];
+                pair4[1] = i;
+                break;
+            }
+        } 
+
+        maxDist = Math.max(maxDist, Math.abs(pair3[1] - pair4[1]));
         return maxDist;
     }
 }
