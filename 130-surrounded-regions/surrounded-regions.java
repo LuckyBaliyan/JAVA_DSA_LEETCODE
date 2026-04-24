281e33978ba3edd7d3c167c328121cbcd5d75a4a
Lucky Baliyan <baliyanlucky85@gmail.com>
2026-04-24 10:38:39 +0530

    Time: 2 ms (77.44%) | Memory: 47.9 MB (24.58%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.java
+++ 130-surrounded-regions/surrounded-regions.java
@@ -1,59 +1,58 @@
 class Solution {
+    static int n, m;
+    static boolean [][] visited;
+
     public void solve(char[][] board) {
-        int n = board.length;
-        int m = board[0].length;
+        n = board.length;
+        m = board[0].length;
 
-        boolean [][] visited = new boolean[n][m];
-
-        //Check for the boundary 0s
-        for(int j = 0;j<m;j++){
-            if(board[0][j] == 'O' && !visited[0][j]){
-                dfs(board,visited,0,j,n,m);
-            }
-
-            if(board[n-1][j] == 'O' && !visited[n-1][j]){
-                dfs(board,visited,n-1,j,n,m);
-            }
-        }
-
-        //for check left, right 0's
-        for(int i = 0;i<n;i++){
+        visited = new boolean [n][m];
+  
+        //cehck and mark boundary 0's as unchangable
+        for(int i = 0; i<n; i++){
             if(board[i][0] == 'O' && !visited[i][0]){
-                dfs(board,visited,i,0,n,m);
-            }
-
-            if(board[i][m-1] == 'O' && !visited[i][m-1]){
-                dfs(board,visited,i,m-1,n,m);
+                dfs(board, i, 0);
+                board[i][0] = 'a';
             }
         }
 
-        for(int i = 0;i<n;i++){
-            for(int j = 0;j<m;j++){
-                if(board[i][j] == 'O' & !visited[i][j]){
+        for(int i = 0; i<n; i++){
+            if(board[i][m-1] == 'O' && !visited[i][m-1]){
+                dfs(board, i, m-1);
+                board[i][m-1] = 'a';
+            }
+        }
+
+        for(int i = 0; i<m; i++){
+            if(board[0][i] == 'O' && !visited[0][i]){
+                dfs(board, 0, i);
+                board[0][i] = 'a';
+            }
+        }
+
+        for(int i = 0; i<m; i++){
+            if(board[n-1][i] == 'O' && !visited[n-1][i]){
+                dfs(board, n-1, i);
+                board[n-1][i] = 'a';
+            }
+        }
+
+        for(int i = 0; i<n; i++){
+            for(int j = 0; j<m; j++){
+                if(board[i][j] == 'O' && !visited[i][j]){
                     board[i][j] = 'X';
                 }
-            }
-        }
-
-        for(int i = 0;i<n;i++){
-            for(int j = 0;j<m;j++){
-                if(board[i][j] == 'a'){
-                    board[i][j] = 'O';
-                }
+                else if (board[i][j] == 'a')board[i][j] = 'O';
             }
         }
     }
 
-    public void dfs(char [][] grid,boolean [][] visited,int i,int j,int n,int m){
-        visited[i][j] = true;
+    public void dfs(char [][] arr, int r, int c){
+        visited[r][c] = true;
 
-        if(i>0 && !visited[i-1][j] && grid[i-1][j] == 'O')
-        dfs(grid,visited,i-1,j,n,m);
-        if(i+1 < n && !visited[i+1][j] && grid[i+1][j]== 'O')
-        dfs(grid,visited,i+1,j,n,m);
-        if(j>0 && !visited[i][j-1] && grid[i][j-1]== 'O')
-        dfs(grid,visited,i,j-1,n,m);
-        if(j+1 < m && !visited[i][j+1] && grid[i][j+1]== 'O')
-        dfs(grid,visited,i,j+1,n,m);
+        if(r - 1 >= 0 && !visited[r-1][c] && arr[r-1][c] == 'O')dfs(arr, r-1, c);
+        if(r+1 < n && !visited[r+1][c] && arr[r+1][c] == 'O')dfs(arr, r+1, c);
+        if(c - 1 >= 0 && !visited[r][c-1] && arr[r][c-1] == 'O')dfs(arr, r, c-1);
+        if(c + 1 < m && !visited[r][c+1] && arr[r][c+1] == 'O')dfs(arr, r, c+1);
     }
 }
