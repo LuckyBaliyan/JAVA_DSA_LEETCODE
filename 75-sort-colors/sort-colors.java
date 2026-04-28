25bb7d6c1a722638c5b4b45117d98eed22676e1f
Lucky Baliyan <baliyanlucky85@gmail.com>
2026-04-28 10:45:29 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (60.86%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,5 +1,28 @@
 class Solution {
     public void sortColors(int[] nums) {
-        Arrays.sort(nums);
+        //Arrays.sort(nums);
+        int k = nums.length - 1;
+        int i = 0;
+        int j = 0;
+
+        while(j<=k){
+            if(nums[j] == 2){
+                swap(j,k,nums);
+                k--;
+            }
+            else if(nums[j] == 0){
+              swap(i,j,nums);
+              i++;
+              j++;
+            }
+            else j++;
+        }
+
+    }
+
+    public void swap(int a, int b, int [] arr){
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
     }
 }
