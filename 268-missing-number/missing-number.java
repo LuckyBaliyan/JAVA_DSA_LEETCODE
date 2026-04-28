6d3afe185168e79c4b0d134c918a8b01cbcc3714
Lucky Baliyan <baliyanlucky85@gmail.com>
2026-04-28 11:13:21 +0530

    Time: 0 ms (100.00%) | Memory: 47.4 MB (48.72%) - LeetSync

--- 268-missing-number/missing-number.java
+++ 268-missing-number/missing-number.java
@@ -2,7 +2,7 @@ class Solution {
     public int missingNumber(int[] nums) {
         int n = nums.length;
 
-        Arrays.sort(nums);
+        /*Arrays.sort(nums);
 
         if(nums[0] != 0)return 0;
         if(nums[n-1] != n)return n;
@@ -11,6 +11,14 @@ class Solution {
             if(nums[i] != i)return i;
         }
 
-        return -1;
+        return -1;*/
+
+        //sum of n natural nums
+        int target = n * ( n + 1 ) / 2;
+        int calculated = 0;
+
+        for(int num : nums)calculated += num;
+
+        return target - calculated;
     }
 }
