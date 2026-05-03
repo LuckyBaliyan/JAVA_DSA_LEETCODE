e1194dfa2e77badb4ad66f00950a51355fa6eec6
Lucky Baliyan <baliyanlucky85@gmail.com>
2026-05-03 17:38:15 +0530

    Time: 16 ms (5.68%) | Memory: 101.5 MB (10.26%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.java
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -9,72 +9,21 @@
  * }
  */
 class Solution {
-    public ListNode left;
     public boolean isPalindrome(ListNode head) {
-        left = head;
-        return solve(head);
+        ListNode last = head;
+        Stack<Integer> st = new Stack<>(); 
 
-       /* Stack<Integer> st = new Stack<>();
-        ListNode temp = head;
-
-        while(temp != null){
-            st.push(temp.val);
-            temp = temp.next;
+        while(last != null)
+        {
+            st.add(last.val);
+            last = last.next;
         }
 
-        ListNode curr = head;
-
-        while(curr != null && !st.isEmpty()){
-            if(curr.val != st.pop())return false;
-
-            curr = curr.next;
-        }
-
-        return true;
-        */
-
-    }
-
-    public static boolean solve(ListNode head){
-        ListNode fast = head;
-        ListNode slow = head;
-
-        while(fast.next != null && fast.next.next != null){
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-
-        ListNode newHead = reverse(slow.next);
-        ListNode first = head;
-        ListNode sec = newHead;
-
-        while(sec != null){
-            if(first.val != sec.val)return false;
-
-            sec = sec.next;
-            first = first.next;
+        while(head != null && !st.isEmpty()){
+            if(head.val != st.pop())return false;
+            head = head.next;
         }
 
         return true;
     }
-
-    public static ListNode reverse(ListNode head){
-        if(head == null || head.next == null)return head;
-        ListNode newHead = reverse(head.next);
-        ListNode front = head.next;
-        front.next = head;
-        head.next = null;
-        return newHead;
-    }
-
-   /* public boolean solve(ListNode right){
-        if(right == null)return true;
-        if(!solve(right.next))return false;
-        if(left.val != right.val)return false;
-        
-        left = left.next;
-        return true;
-    }
-    */
-
 }
