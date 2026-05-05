1c6c6793d8f858595ce15a54b12818e3ba7bb4fc
Lucky Baliyan <baliyanlucky85@gmail.com>
2026-05-05 22:25:26 +0530

    Time: 1 ms (58.91%) | Memory: 48.5 MB (28.60%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,39 +1,24 @@
 class Solution {
     public int trap(int[] height) {
+        //Brute Force Approach 
         int n = height.length;
-        int water = 0;
+
+        int water  = 0;
 
         int [] leftMax = new int [n];
-        int [] rightMax = new int [n];
-
         leftMax[0] = height[0];
+
+        for(int i = 1; i<n; i++)leftMax[i] = Math.max(leftMax[i-1], height[i]);
+
+        int [] rightMax = new int [n];
         rightMax[n-1] = height[n-1];
 
-        for(int i = 1; i<n; i++){
-            leftMax[i] = Math.max(leftMax[i-1], height[i]);
-        }
+        for(int i = n-2; i>=0; i--)
+        rightMax[i] = Math.max(rightMax[i+1], height[i]);
 
-        for(int i = n-2; i >= 0; i--){
-            rightMax[i] = Math.max(rightMax[i+1], height[i]);
-        }
 
 
         for(int i = 0; i<n; i++){
-           /* int leftMax = 0;
-            int rightMax = 0;*/
-
-            //for each index find its left max and right maxboundary 
-            // and - that height as well | - |
-            
-            /*
-            for(int  j = 0;j <= i; j++){
-               leftMax = Math.max(leftMax, height[j]);
-            }
-
-            for(int  j = i;j < n; j++){
-               rightMax = Math.max(rightMax, height[j]);
-            }*/
-
             water += Math.min(leftMax[i], rightMax[i]) - height[i];
         }
 
