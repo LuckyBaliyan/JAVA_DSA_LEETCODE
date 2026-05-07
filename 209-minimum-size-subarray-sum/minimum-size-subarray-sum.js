0847022d4bbfdf70d05ecbe954e730d20cc217e2
Lucky Baliyan <baliyanlucky85@gmail.com>
2026-05-07 21:39:20 +0530

    Time: 2 ms (86.07%) | Memory: 57.8 MB (81.50%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.js
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.js
@@ -4,19 +4,23 @@
  * @return {number}
  */
 var minSubArrayLen = function(target, nums) {
-    const n = nums.length;
     let minLen = Number.MAX_VALUE;
+    let n = nums.length;
+
     let left = 0;
     let sum = 0;
 
-    for(let right  = 0; right < n; right++){
+    for(let right = 0; right < n; right++){
         sum += nums[right];
 
         while(sum >= target){
-            minLen = Math.min(minLen, right - left + 1);
-            sum -= nums[left++];
+            let len = right - left + 1;
+            minLen = Math.min(minLen, len);
+
+            sum -= nums[left];
+
+            left++;
         }
-        
     }
 
     return minLen == Number.MAX_VALUE ? 0 : minLen;
