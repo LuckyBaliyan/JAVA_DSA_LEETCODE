7f5ab860ad9dec93ec08ada7582a703509edb3d6
Lucky Baliyan <baliyanlucky85@gmail.com>
2026-06-17 00:45:50 +0530

    Time: 1 ms (38.07%) | Memory: 46.9 MB (42.16%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,39 +1,39 @@
 class Solution {
-    static int [] dr = {-1, 0, 1, 0};
-    static int [] dc = {0,-1,0,1};
+    public static int [][] visited;
+    int [] dr = {-1,1,0,0};
+    int [] dc = {0,0,-1,1};
 
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-       int n = image.length;
-       int m = image[0].length;
+        int n = image.length;
+        int m = image[0].length;
 
-       boolean [][] visited = new boolean [n][m];
-       Queue<int []> q = new LinkedList<>();
+        int originalColor = image[sr][sc];
 
-       int originalColor = image[sr][sc];
+        visited = new int [n][m];
 
-       q.offer(new int [] {sr,sc,color});
-       visited[sr][sc] = true;
-       image[sr][sc] = color;
+        Queue<int []> q = new LinkedList<>();
+        q.offer(new int [] {sr,sc,color});
+        visited[sr][sc] = 1;
+        image[sr][sc] = color;
 
-       while(!q.isEmpty()){
-          int [] curr = q.poll();
-          int r = curr[0];
-          int c = curr[1];
-          int Prevcolor = curr[2];
+        while(!q.isEmpty()){
+            int [] curr = q.poll();
+            int r = curr[0];
+            int c = curr[1];
 
-          for(int i = 0; i<4; i++){
-            int nr = r + dr[i];
-            int nc = c + dc[i];
+            for(int i = 0; i<4; i++){
+                int nr = r + dr[i];
+                int nc = c + dc[i];
 
-            if(nr >=0 && nr < n && nc >=0 && nc < m && 
-            image[nr][nc] == originalColor && !visited[nr][nc]){
-                visited[nr][nc] = true;
-                image[nr][nc] = Prevcolor;
-                q.offer(new int [] {nr,nc,Prevcolor});
+                if(nr > - 1 && nr < n && nc > -1 && nc < m &&
+                visited[nr][nc] == 0 && image[nr][nc] == originalColor){
+                    image[nr][nc] = curr[2];
+                    visited[nr][nc] = 1;
+                    q.offer(new int [] {nr,nc,curr[2]});
+                }
             }
-          }
-       }
+        }
 
-       return image;
+        return image;
     }
 }
